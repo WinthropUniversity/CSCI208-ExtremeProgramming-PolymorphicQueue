58d49d961e7c0fe5da58f06afd5044f637922392
jimenezn3 <jimenezn3@mailbox.winthrop.edu>
2020-11-02 12:53:28 -0500

    Circle.h and initial Circle.cc files

---
+++ inc/Circle.h
@@ -0,0 +1,21 @@
+#ifndef CIRCLE_H_
+#define CIRCLE_H_
+#define PI 3.14
+
+#include <Shape.h>
+
+class Circle : public Shape {
+
+  public:
+	Circle(double radius);
+	GetRadius();
+	SetRadius(double radius);
+	GetArea();
+	GetPerimeter();
+
+  protected:
+	radius_;
+
+};
+
+#endif //CIRCLE_H_
---
+++ src/Circle.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <Circle.h>
+
+using namespace std;
+
+void Circle::SetRadius(double radius) {
+  radius_ = radius;
+}
+
+double Circle::GetRadius() {
+  return radius_;
+}
+
+double Circle::GetArea() {
+
+
+}
+
