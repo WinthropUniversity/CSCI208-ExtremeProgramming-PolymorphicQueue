1e8977c758bae6d50e70a9fb99cf469f1660ee9c
jimenezn3 <jimenezn3@mailbox.winthrop.edu>
2020-11-04 12:42:20 -0500

    added AskForShapeParameters() & PrintShapeInfo()

--- src/Circle.cc
+++ src/Circle.cc
@@ -1,27 +1,51 @@
 #include <iostream>
+#include <string>
 #include <Circle.h>
 
 
+//Constructor
+Circle::Circle(){
 
-Circle::Circle(double radius){
-
-  radius_ = radius;
+  radius_ = 0.0;
 }
 
-/* void Circle::SetRadius(double radius) {
+//Accessor Methods
+void Circle::AskUserForShapeParameters() {
+  //Get the circle's radius
+  cout << "What is your circle's radius? ";
+  double userRadius;
+  cin >>  userRadius;
+  this->SetRadius(userRadius);
+
+  //Get the circle's color
+  cout << "What is your circle's color? ";
+  string circleColor;
+  cin >> circleColor;
+  this->SetColor(circleColor);
+}
+
+
+void Circle::SetRadius(double radius) {
   radius_ = radius;
-} */
+}
 
 double Circle::GetRadius() {
   return radius_;
 }
 
 double Circle::GetArea() {
-
   return radius_ * radius_ * PI;
 }
 
 double Circle::GetPerimeter() {
-
   return 2.0 * PI * radius_;
+}
+
+void PrintShapeInfo() {
+  cout << "  Shape: Circle" << endl;
+  cout << "  Color " << this-> GetColor() << endl;
+  cout << "  Radius: " << this->GetRadius() << endl;
+  cout << "  Area: " << this->GetArea() << endl;
+  cout << "  Perimeter: " << this->GetPerimeter() << endl;
+
 }
