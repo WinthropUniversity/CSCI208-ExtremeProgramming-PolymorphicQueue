3a349cef458f3cbb87d8dcfaeca21a886eec3f0f
JhetSquared <70283444+JhetSquared@users.noreply.github.com>
2020-11-04 12:59:19 -0500

    Create Rectangle.h

---
+++ inc/Rectangle.h
@@ -0,0 +1,28 @@
+#ifndef RECTANGLE_H_
+#define RECTANGLE_H_
+#include<iostream>
+#include<Shape.h>
+
+using namespace std;
+
+class Rectangle: public Shape{
+
+public:
+double GetLength();
+double SetLength(double Length);
+
+double GetWidth();
+double SetWidth(double Width);
+
+double GetArea();
+double GetPerimeter();
+
+void PrintShapeInfo();
+void AskUseForShapePerimeters();
+
+protected:
+double length;
+double width;
+
+};
+#endif
