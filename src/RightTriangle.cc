8e54ea01574390a8f13a84029e44b33a9f53a3c5
John Herndon <johnherndon@Johns-MacBook-Pro.local>
2020-11-04 13:26:32 -0500

    Kailey's Commit

--- src/RightTriangle.cc
+++ src/RightTriangle.cc
@@ -1,6 +1,7 @@
-#include <RightTraingle.h>
+#include <RightTriangle.h>
 
-// To-Do: Complete functions for body file 
+// To-Do: Complete functions for body file
+using namespace std;
 
 double GetBase() {
 	return inBase; }
@@ -19,8 +20,20 @@ double GetArea() {
 double GetPerimeter() {
 
 void PrintShapeInfo() {
+cout << "Shape: Right Triangle" << endl;
+cout << "Base: " << this->GetBase() << endl;
+cout << "Height: " << this->GetArea() << endl;
+cout << "Area: " << this->GetArea() << endl;
+cout << "Perimeter: " << this->GetPerimeter() << endl;
+}
 
 void AskUserForShapeParameters() {
-
-RightTriangle() {
-
+cout << "What is the base of the triangle?" << endl;
+double inBase;
+cin >> inBase;
+this->SetBase(inBase);
+cout << "What is the height of the triangle?" << endl;
+double inHeight;
+cin >> inHeight;
+this->SetHeight(inHeight);
+}
