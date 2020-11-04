68264cc1f216f4d25fe8c0387b6c95707492028b
John Herndon <johnherndon@johns-mbp.win.winthrop.edu>
2020-11-04 11:09:52 -0500

    Second Try

---
+++ inc/RightTriangle.h
@@ -0,0 +1,28 @@
+#ifndef RIGHTTRIANGLE_H_
+#define RIGHTTRIANGLE_H_
+#define rightAngle 90
+
+#include<iostream>
+#include<Shape.h> //Mathew's File
+
+using namespace std;
+
+class RightTriangle : public Shape {
+
+public:
+  double GetBase();
+  double SetBase(double inBase);
+  double GetHeight();
+  double SetHeight(double inHeight);
+  double GetArea();
+  double GetPerimeter();
+
+  RightTriangle(double inBase, double inHeight);
+
+protected:
+  double base; //Base gotten from user
+  double height; //Height gotten from user
+
+}
+
+#endif
