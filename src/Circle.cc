0eb2983b4e7c93b1a3b793e1adccdb8ce8f6835e
R. Paul Wiegand <rpwiegand@gmail.com>
2020-11-04 11:55:37 -0500

    Fixed const declarations for consistency

--- src/Circle.cc
+++ src/Circle.cc
@@ -16,12 +16,12 @@ double Circle::GetRadius() {
   return radius_;
 }
 
-double Circle::GetArea() const {
+double Circle::GetArea() {
 
-  return radius_ * radius_ * PI; 
+  return radius_ * radius_ * PI;
 }
 
-double Circle::GetPerimeter() const {
+double Circle::GetPerimeter() {
 
   return 2.0 * PI * radius_;
 }
