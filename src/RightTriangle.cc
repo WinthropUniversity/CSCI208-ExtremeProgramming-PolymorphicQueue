38903ae5708cb9873ac77902bec5edb5aa6b2ff6
MatthewHoshauer <hoshauerm2@mailbox.winthrop.edu>
2020-11-04 13:05:36 -0500

    Did getter / setter functions

--- src/RightTriangle.cc
+++ src/RightTriangle.cc
@@ -3,17 +3,23 @@
 // To-Do: Complete functions for body file 
 
 double GetBase() {
+	return inBase; }
 
 double SetBase(double inBase) {
+	base = inBase; }
 
 double GetHeight() {
+	return inHeight; }
 
 double SetHeight(double inHeight) {
+	height = inHeight; }
 
 double GetArea() {
+
 double GetPerimeter() {
 
 void PrintShapeInfo() {
+
 void AskUserForShapeParameters() {
 
 RightTriangle() {
