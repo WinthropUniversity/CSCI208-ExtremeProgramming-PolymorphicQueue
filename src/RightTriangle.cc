9a2809be764157af580a58baf514a909c8e248b6
MatthewHoshauer <hoshauerm2@mailbox.winthrop.edu>
2020-11-04 12:55:27 -0500

    Adding body file for Right Triangle

---
+++ src/RightTriangle.cc
@@ -0,0 +1,20 @@
+#include <RightTraingle.h>
+
+// To-Do: Complete functions for body file 
+
+double GetBase() {
+
+double SetBase(double inBase) {
+
+double GetHeight() {
+
+double SetHeight(double inHeight) {
+
+double GetArea() {
+double GetPerimeter() {
+
+void PrintShapeInfo() {
+void AskUserForShapeParameters() {
+
+RightTriangle() {
+
