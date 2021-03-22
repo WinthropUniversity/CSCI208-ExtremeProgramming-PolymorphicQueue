7697014aee10a470957a0a38a9368f40cf131cd2
lloydb4 <60805874+lloydb4@users.noreply.github.com>
2021-03-22 16:39:19 -0400

    Update queue-main.cc

--- src/queue-main.cc
+++ src/queue-main.cc
@@ -1,8 +1,30 @@
+#include<iostream>
+//#include<vector>
+
 #include<ShapeQueue.h>
-// Others??
+#include<Shape.h>
 
-int main() {
 
+using namespace std;
+
+int main(int argc, char **argv) {
+  /* First get shape(s) info - do you I decalre random dimensions for the shapes or is this user inputed data? */
+  Circle myCircle(0.0);                      //Parameters -- Radius?
+  //SemiCircle mySemiCircle(0.0);            //Parameters -- 
+  
+  Triangle myTriangle(0.0, 0.0);                  //Parameters -- Base & Height?
+  //EqualateralTriangle myEqTriangle(0.0, 0.0);   //Parameters -- 
+  
+  Rectangle myRectangle(0.0, 0.0);        //Parameters -- Length & Width? 
+  Square mySquare();                      //Parameters -- 
+  
+  
+  /* Store declared shape(s) in Queue */
+
+  
+  /* Print the Queue */
+  
+  
   // Everything is okay
   return 0;
 }
