a6abb7554e5fd09b19bdd08123f6bcbddd161ce7
cobbinag2 <cobbinag2@mailbox.winthrop.edu>
2021-03-22 12:55:23 -0400

    Added necessary header file components

--- inc/rightTriangle.h
+++ inc/rightTriangle.h
@@ -1,1 +1,11 @@
 // Right triangle header file
+#ifndef RIGHTTRIANGLE_H_
+#define RIGHTTRIANGLE_H_
+
+#include <iostream>
+#include <triangle.h> // Including the header file for the base class
+#include <cmath> // Necessary for sqrt() and pow() functions
+
+using namespace std;
+
+#endif
