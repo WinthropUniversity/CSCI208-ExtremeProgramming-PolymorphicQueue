5dee6d502ebd0b0443a376210f2e580366ff5b7d
lloydb4 <60805874+lloydb4@users.noreply.github.com>
2021-03-22 17:20:01 -0400

    Update queue-main.cc

--- src/queue-main.cc
+++ src/queue-main.cc
@@ -8,18 +8,28 @@ using namespace std;
 
 int main(int argc, char **argv) {
   // --------------------------------------------------------------------------------------------------------------  //
-  /* First get shape(s) info - do you I decalre random dimensions for the shapes or is this user inputed data?   */
+  /* First get shape(s) info - do you I decalre random dimensions for the shapes or is this user inputed data? 
+     Would it perhaps be better to call an instance of ShapeQueue and store each individual shape (myCircle, etc.) into that queue?
+  */
+  
+  Shape
+  
   Circle myCircle(0.0);                      //Parameters -- Radius?
   //SemiCircle mySemiCircle(0.0);            //Parameters -- 
+  //InsertShape(myCircle);
+  //InsertShape(
+  
   
   Triangle myTriangle(0.0, 0.0);                  //Parameters -- Base & Height?
   //EqualateralTriangle myEqTriangle(0.0, 0.0);   //Parameters -- 
+  //InsertShape(myTriangle*);
+  //InsertShape(myEqTriangle*);
   
-  Rectangle myRectangle(0.0, 0.0);        //Parameters -- Length & Width? 
-  Square mySquare();                      //Parameters -- 
+  Rectangle myRectangle(0.0, 0.0);                  //Parameters -- Length & Width? 
+  //Square mySquare(0.0, 0.0);                      //Parameters -- 
   
   // --------------------------------------------------------------------------------------------------------------  //
-  /* Create a generic vector of shapes and insert each shape into the queue? */
+  /* Create a generic vector of shapes and insert each shape into the queue via loop?  */
   Vector<Shape*> ShapeList; 
   
   ShapeList.push_back(new Circle(0.0));
