df584537c48723634936655ddc5776cfe2e7c127
Forrest Immel <immelf2@mailbox.winthrop.edu>
2021-03-22 13:10:03 -0400

    added Triangle.h

---
+++ inc/Triangle.h
@@ -0,0 +1,18 @@
+#ifndef TRIANGLE_
+#define TRIANGLE_
+
+#include<iostream>
+using namespace std;
+
+class Triangle
+{
+   private:
+      double base;
+      double height;
+
+   public:
+      Triangle();
+      double GetBase();
+      double GetHeight();
+}
+
