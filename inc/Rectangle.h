372f59b42112b1067cdefbfc8eae6acdabf88e11
melo_yelo <martinm43@mailbox.winthrop.edu>
2021-03-22 13:15:31 -0400

    made Rectangle.h and Rectangle.cc

---
+++ inc/Rectangle.h
@@ -0,0 +1,26 @@
+#ifndef Rectangle.h
+#define Rectangle.h
+#incude shape.h
+
+
+using namespace std;
+
+class Rectangle : public Shape {
+
+private:
+
+double Width;
+
+double Length;
+
+
+public:
+
+double GetWidth();
+double GetLength();
+
+
+
+
+
+}
---
+++ src/Rectangle.cc
@@ -0,0 +1,23 @@
+#include <Rectangle.h>
+
+#include <iostream>
+
+using namespace std;
+
+double Rectangle(){
+
+  double Rectangle::GetWidth(){
+
+
+  }
+
+
+    double Rectangle::GetLength(){
+
+
+    }
+
+
+
+
+}
