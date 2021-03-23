653380656beae9fe4e4a98799f104efb32696d76
lloydb4 <60805874+lloydb4@users.noreply.github.com>
2021-03-23 14:31:47 -0400

    Update queue-main.cc

--- src/queue-main.cc
+++ src/queue-main.cc
@@ -1,8 +1,11 @@
 #include<iostream>
-#include<vector>
-
 #include<ShapeQueue.h>
 #include<Shape.h>
+#include<RightTriangle.h>
+#include<Circle.h>
+#include<Rectangle.h>
+#include<SemiCircle.h>
+#include<EqualateralTriangle.h>
 
 using namespace std;
 
@@ -14,48 +17,20 @@ using namespace std;
   **/
 int main(int argc, char **argv) {
   
-  /* First get shape(s) info - Should I decalre random dimensions for the shapes or is this user inputed data? 
-     Would it perhaps be better to call an instance of ShapeQueue and store each individual shape (myCircle, etc.) into that queue? */
-  
-  ShapeQueue Queue;         //Create a ShapeQueue, and populate it
-  
-  //Circle myCircle(0.0);                      //Parameters -- Radius?
-  //SemiCircle mySemiCircle(0.0);            //Parameters -- 
-  Queue.InsertShape(myCircle);
-  Queue.InsertShape(mySemiCircle);
-  
-  //Triangle myTriangle(0.0, 0.0);                  //Parameters -- Base & Height?
-  //EqualateralTriangle myEqTriangle(0.0, 0.0);   //Parameters -- 
-  Queue.InsertShape(myTriangle*);
-  Queue.InsertShape(myEqTriangle*);
-  
-  //Rectangle myRectangle(0.0, 0.0);                  //Parameters -- Length & Width? 
-  //Square mySquare(0.0, 0.0);                      //Parameters -- 
-  Queue.InsertShape(myRectangle*);
-  Queue.InsertShape(mySquare*);
-  
+  /* Create a ShapeQueue, and populate it */
+  ShapeQueue Queue;
+    
+  /* Pointers to Shapes are created and filled with random values for testing */
+  Queue.InsertShape(new Circle(15.4));
+  Queue.InsertShape(new SemiCircle(7.5));
+  Queue.InsertShape(new RightTriangle(6.0, 4.2));
+  Queue.InsertShape(new EqualateralTriangle(2.0, 3.9));
+  Queue.InsertShape(new Rectangle(24.7, 12.8));                  
+  Queue.InsertShape(new Square(5.0, 8.0));
+
   /* Print all the shape info */
   Queue.PrintAllShapes();
-  
-  // --------------------------------------------------------------------------------------------------------------  //
-  /* Create a generic vector of shapes and insert each shape into the queue via loop?  */
-  Vector<Shape*> ShapeList; 
-  
-  ShapeList.push_back(new Circle(0.0));
-  ShapeList.push_back(new SemiCircle(0.0));
-  ShapeList.push_back(new Triangle(0.0, 0.0));
-  ShapeList.push_back(new EqualateralTriangle(0.0, 0.0));
-  ShapeList.push_back(new Rectangle(0.0, 0.0));                  
-  ShapeList.push_back(new Square(0.0, 0.0));
-  
-  /* Store declared shape(s) in Queue.  Question -- is this operation possible? */
-  for (int idx=0; idxSshapeList.size(); idx++)
-    ShapeList.InsertShape(idx);  
-  
-  /* Print all the shape info */
-  
-  
-  // --------------------------------------------------------------------------------------------------------------  //
+    
   // Everything is okay
   return 0;
 }
