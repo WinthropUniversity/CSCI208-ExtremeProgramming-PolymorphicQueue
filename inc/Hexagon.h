0b3c206f630c70232457183c02a94392f2f29421
owens6 <elowens02@icloud.com>
2021-03-24 12:33:54 -0400

    Finished most of the hexagon derived class, should work as intended.

--- inc/Hexagon.h
+++ inc/Hexagon.h
@@ -1,3 +1,7 @@
+/**
+  Header file of Hexagon for a polymorphic queue that outputs
+  the area and the perimeter of different shapes.
+**/
 #ifndef HEXAGON_
 #define HEXAGON_
 
@@ -6,5 +10,17 @@
 
 using namespace std;
 
+class Hexagon : public Shape  {
+public:
+  Hexagon(double length);
+
+  virtual double GetArea() const;
+  virtual double GetPerimeter() const;
+
+protected:
+  double length_;
+
+  void PrintParamLine() const;
+};
 
 #endif
