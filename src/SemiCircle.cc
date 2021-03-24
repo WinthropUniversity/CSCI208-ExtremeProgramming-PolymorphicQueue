4c7f72c3f80cb4aff857d67a138f6beff21bd174
Nicolas Diaz <diazn3@winthrop.edu>
2021-03-24 11:53:59 -0400

    Added override functions to SemiCircle.cc

--- src/SemiCircle.cc
+++ src/SemiCircle.cc
@@ -1,1 +1,17 @@
 //this is SemiCircle.cc
+
+#include<SemiCircle.h>
+
+#define PI 3.141592653589
+
+SemiCircle::SemiCircle(double inRadius) : radius_(inRadius) {
+  shapeName_ = "SemiCircle";
+}
+
+double SemiCircle::GetArea() const {
+  return radius_ * radius_ * PI;
+}
+
+double SemiCircle::GetPerimeter() const {
+  return 2.0* radius_ * PI;
+}
