53980213a113f8032741e27dece066633b384f2d
Sky Minton <77761666+SkyMinton@users.noreply.github.com>
2021-03-24 12:06:30 -0400

    Update ShapeQueue.cc

--- src/ShapeQueue.cc
+++ src/ShapeQueue.cc
@@ -38,14 +38,22 @@ Shape *ShapeQueue::GetShape() {
   // Decrement the size_
   // What do we do when the list is empty???
   // Deallocate the ShapeNode
-  if (headPtr_ == NULL)
+	
+  /*if (headPtr_ == NULL){
+	  return NULL;//list is empty, return nothing
+  } */
+  //is this necessary?
+  //singular node handle
+  if (headPtr_->GetNextNode() == NULL)//only one node in list if the next node is NULL
   {
-     tailPtr_ = headPtr_;
-     return tailPtr_;
+	tailPtr_ = headPtr_;
+	return headPtr_;
   }
+	
   ShapeNode* curNode = headPtr_;//get the current headNode
-  headPtr_ = headPtr_->GetNextNode(); //set the headPtr to the next node in the list
-  /*delete headPtr_;*/ //delete the current Node??
+  //tail pointer points to last element in the list
+  headPtr_ = (tailPtr_ = headPtr_->GetNextNode()); //set the headPtr to the next node in the list
+  size_--;//decremet per comment
   return curNode; //return the current Node
 }
 
